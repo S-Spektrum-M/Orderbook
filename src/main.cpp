c9490ade6eee9768c7a61b1323a6082da34caf85
Siddharth Mohanty <neosiddharth+git@gmail.com>
2024-12-18 00:11:53 -0800

    feat: allow user to pass in desired port through terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "../includes/Server.hpp"
-#include <thread>
 
-int main() {
-    startServer();
+int main(int argc, char **argv) {
+    if (argc)
+        startServer(atoi(argv[0]));
+    else
+        startServer();
 }
