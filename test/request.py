bcb7b5bfa51b75d5860c618a1a0e26bc2617ee13
Siddharth Mohanty <neosiddharth+git@gmail.com>
2024-12-25 19:49:45 -0800

    fix: proper tests

--- test/request.py
+++ test/request.py
@@ -1,33 +1,47 @@
-import time
+import sys
 import random
 import requests
-import subprocess
 
-# subprocess.Popen(["make", "run"])
+if len(sys.argv) < 2:
+    PORT = input("Port: ") or 8080
+else:
+    PORT = sys.argv[1]
 
-time.sleep(1)
-
-PORT = input("Port: ") or 8080
 TEST_TICKER = 'MSFT'
-prices = [int(random.random() * 100)/100 + 300 for i in range(100)]
-quantities = [random.randint(1, 10000) for i in range(1000)]  # generate list of random ints
+test_ct = 100_000
 
-buy_order = [
-    f'http://localhost:{PORT}/buy_order?ticker={TEST_TICKER}&price={price}&quantity={quantity}'
-    for price, quantity in zip(prices, quantities)
-]
+prices = [int(random.random() * 100)/100 + 300 for i in range(test_ct)]
 
-for buy in buy_order:
-    requests.get(buy)
-
-sell_order = [
-    f'http://localhost:{PORT}/sell_order?ticker={TEST_TICKER}&price={price}&quantity={quantity}'
-    for price, quantity in zip(prices, quantities)
-]
-
-for sell in sell_order:
-    requests.get(sell)
+# generate list of random ints
+quantities = [random.randint(1, 10000) for i in range(test_ct)]
 
 
-res = requests.get(f'http://localhost:{PORT}/all_orders?ticker={TEST_TICKER}').json()
+def check():
+    buy_order = [
+        f'http://localhost:{PORT}/buy_order?ticker={TEST_TICKER}&price={price}'
+        f'&quantity={quantity}'
+        for price, quantity in zip(prices, quantities)
+    ]
 
+    for buy in buy_order:
+        requests.get(buy)
+
+    sell_order = [
+        f'http://localhost:{PORT}/sell_order?ticker={TEST_TICKER}'
+        f'&price={price}&quantity={quantity}'
+        for price, quantity in zip(prices, quantities)
+    ]
+
+    for sell in sell_order:
+        requests.get(sell)
+
+    res = requests.get(f'http://localhost:{PORT}/all_orders?'
+                       f'ticker={TEST_TICKER}').json()
+    for x in res:
+        for y in res[x]:
+            for z in res[x][y]:
+                if z != []:
+                    raise Exception
+
+
+check()
