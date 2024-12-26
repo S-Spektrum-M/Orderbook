7cc8fda56e316572f2532026ceca469ca69ef9cd
Siddharth Mohanty <neosiddharth+git@gmail.com>
2024-12-25 19:49:24 -0800

    simplify start command

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include "../includes/Server.hpp"
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    if (argc == 2)
-        startServer(atoi(argv[1]));
-    else
-        startServer(8080);
+    startServer(argc == 2 ? atoi(argv[1]) : 80 + !(geteuid() == 0) * 8000);
 }
